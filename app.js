ad0040608a9209b14c9752418d19b071c623e2b7
Alex Hoeing <lhoeing@msn.com>
2016-01-27 13:43:35 +0100

    node

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+var server = app.listen(1337, 'localhost', function(){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Listening at ' + host + ':' + port);
+});
