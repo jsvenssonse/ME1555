4d5a8e812bb01a35f35fd1273b6fdeaaa9c6f639
xonvanetta <xon.vanetta@gmail.com>
2016-02-03 14:52:46 +0100

    kiwi

--- phpserver/index.php
+++ phpserver/index.php
@@ -4,9 +4,12 @@ require_once __DIR__ . '/Core.php';
 
 $Core = new Core();
 
+var_dump(file_get_contents("php://input"));
+echo "<hr>";
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     //only if post exist
-    $Core->Fetch(file_get_contents("php://input"));
+    //$Core->Fetch(file_get_contents("php://input"));
 } else {
     $Core->DisplayXml();
 }
