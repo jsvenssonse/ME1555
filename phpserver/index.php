fb1a03283ccd2d99288eea7835a6bcf99f10cafc
xonvanetta <xon.vanetta@gmail.com>
2016-02-03 14:48:12 +0100

    phpserver fixes

--- phpserver/index.php
+++ phpserver/index.php
@@ -4,12 +4,9 @@ require_once __DIR__ . '/Core.php';
 
 $Core = new Core();
 
-if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest')
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest'){
     //only if post exist
     $Core->Fetch(file_get_contents("php://input"));
 } else {
     $Core->DisplayXml();
 }
-
-
-['json']  => json obj
