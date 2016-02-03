430063ea12ec3682ae429de1a3badbed49ee0f38
xonvanetta <xon.vanetta@gmail.com>
2016-02-03 14:31:29 +0100

    phpserver fixes

--- phpserver/index.php
+++ phpserver/index.php
@@ -3,16 +3,11 @@ echo header("Access-Control-Allow-Origin: *");
 require_once __DIR__ . '/Core.php';
 
 $Core = new Core();
-/*
-$_POST['json'] = '{"lang" : "python"}';
-$Core->Fetch($_POST['json']);
-*/
 
 var_dump(file_get_contents("php://input"));
-
 echo '<hr>';
-
 var_dump($_POST);
+echo '<hr>';
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     //only if post exist
     //$Core->Fetch($_POST['json']);
