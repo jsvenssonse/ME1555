e684faf3c154172df4d07e80f64c7397e20da01f
xonvanetta <xon.vanetta@gmail.com>
2016-02-03 15:01:38 +0100

    fixes

--- phpserver/index.php
+++ phpserver/index.php
@@ -3,7 +3,7 @@ echo header("Access-Control-Allow-Origin: *");
 require_once __DIR__ . '/Core.php';
 
 $Core = new Core();
-
+    
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     //only if post exist
     $Core->Save("javascript");
